0caa1f3452dae76619c2d7ea1df88a9c427da514
Joshua <jla747@sfu.ca>
2021-11-01 15:04:17 -0700

    Implement Child and ChildManager.
    Child has a name field, ChildManager stores Child and has methods
     for accessing.

--- app/src/main/java/ca/sfu/cmpt276/be/parentapp/model/Child.java
+++ app/src/main/java/ca/sfu/cmpt276/be/parentapp/model/Child.java
@@ -1,4 +1,16 @@
 package ca.sfu.cmpt276.be.parentapp.model;
 
+/**
+ * Child represents a child in the app.
+ */
 public class Child {
+    private final String name;
+
+    public Child(String givenName) {
+        this.name = givenName;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
