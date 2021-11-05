2c379ebde6c04b283ad26486019e46280e3093ce
Jacky Lim <jackydroid68@gmail.com>
2021-11-05 01:51:48 -0700

    Added functionality to remember who last picked.

--- app/src/main/java/ca/sfu/cmpt276/be/parentapp/CoinFlipActivity.java
+++ app/src/main/java/ca/sfu/cmpt276/be/parentapp/CoinFlipActivity.java
@@ -17,8 +17,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
 
-import org.w3c.dom.Text;
-
 import java.util.Objects;
 
 import ca.sfu.cmpt276.be.parentapp.databinding.ActivityCoinflipBinding;
@@ -27,13 +25,12 @@ import ca.sfu.cmpt276.be.parentapp.model.CoinFlipManager;
 
 public class CoinFlipActivity extends AppCompatActivity implements CoinFlipManager.CoinObserver {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityCoinflipBinding binding;
-
-    private ImageView coin;
     MediaPlayer player;
     CoinFlipManager coinFlipManager = CoinFlipManager.getInstance();
     ChildManager childManager = ChildManager.getInstance();
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityCoinflipBinding binding;
+    private ImageView coin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +43,12 @@ public class CoinFlipActivity extends AppCompatActivity implements CoinFlipManag
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        initializeCoin();
         setupButton();
     }
 
     private void setupButton() {
+
+        coin = (ImageView) findViewById(R.id.coin_image);
 
         Button flipButton = (Button) findViewById(R.id.flipCoinButton);
         flipButton.setOnClickListener(view -> {
@@ -75,30 +73,9 @@ public class CoinFlipActivity extends AppCompatActivity implements CoinFlipManag
             nextChild.setVisibility(View.GONE);
         } else {
             flipButton.setVisibility(View.GONE);
-            if (coinFlipManager.getNextChild() >= childManager.getAll().size()){
-                Toast.makeText(this,"list reset", Toast.LENGTH_SHORT).show();
-                coinFlipManager.setNextChild(0);
-            }
-            nextChild.setText("Next child: "+childManager.get(coinFlipManager.getNextChild()).getName());
+
         }
 
-
-    }
-
-    private void initializeCoin() {
-        coin = findViewById(R.id.coin_image);
-        TextView tv = findViewById(R.id.flipResult);
-        String result = coinFlipManager.emptyFlip();
-        switch (result) {
-            case "Heads":
-                coin.setImageResource(R.drawable.coin_heads);
-                tv.setText("Heads");
-                break;
-            case "Tails":
-                coin.setImageResource(R.drawable.coin_tails);
-                tv.setText("Tails");
-                break;
-        }
     }
 
     private void flipCoin(String userChoice) {
@@ -122,7 +99,7 @@ public class CoinFlipActivity extends AppCompatActivity implements CoinFlipManag
             public void onAnimationStart(Animator animation) {
                 super.onAnimationStart(animation);
                 coin.setImageResource(R.drawable.coin_blur);
-                player = MediaPlayer.create(CoinFlipActivity.this,R.raw.coin_sound);
+                player = MediaPlayer.create(CoinFlipActivity.this, R.raw.coin_sound);
                 player.start();
             }
         });
@@ -135,26 +112,36 @@ public class CoinFlipActivity extends AppCompatActivity implements CoinFlipManag
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        if (item.getItemId() == R.id.coinHistoryButton){
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.coinHistoryButton) {
             Intent intent = new Intent(getApplicationContext(), FlipHistoryActivity.class);
             startActivity(intent);
         }
+
         if (item.getItemId() == android.R.id.home) {
             finish();
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
     private void updateDisplayCoinResult() {
+
         TextView tv = findViewById(R.id.flipResult);
-        if (coinFlipManager.getCoinList().size() > 0){
+        if (coinFlipManager.getCoinList().size() > 0) {
             tv.setText(coinFlipManager.getCoinFlipGame(0).getResult());
         }
 
         TextView nextChild = (TextView) findViewById(R.id.coinNextChild);
-        nextChild.setText("Next child: "+childManager.get(coinFlipManager.getNextChild()).getName());
+        if (childManager.getAll().size() != 0) {
+            if (coinFlipManager.getChildIndex() >= childManager.getAll().size()) {
+                Toast.makeText(this, "Child Deleted. Order is reset.", Toast.LENGTH_SHORT).show();
+                coinFlipManager.setChildIndex(0);
+            }
+            nextChild.setText(String.format("%s%s", getString(R.string.nextChild), childManager.get(coinFlipManager.getChildIndex()).getName()));
+        }
 
     }
 
--- app/src/main/java/ca/sfu/cmpt276/be/parentapp/model/CoinFlipManager.java
+++ app/src/main/java/ca/sfu/cmpt276/be/parentapp/model/CoinFlipManager.java
@@ -1,16 +1,12 @@
 package ca.sfu.cmpt276.be.parentapp.model;
 
-import android.widget.Toast;
+import androidx.annotation.Nullable;
 
-import java.lang.reflect.Array;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
-
-import ca.sfu.cmpt276.be.parentapp.CoinFlipActivity;
 
 public class CoinFlipManager {
 
@@ -18,20 +14,9 @@ public class CoinFlipManager {
 
     ArrayList<Coin> coinFlipHistory = new ArrayList<Coin>();
 
-    public int getNextChild() {
-        return nextChild;
-    }
-
-    public void setNextChild(int nextChild) {
-        this.nextChild = nextChild;
-    }
-
-    int nextChild = 0;
-    private List<CoinObserver> observers = new ArrayList<>();
-
-
+    int childIndex = 0;
     ChildManager childManager = ChildManager.getInstance();
-
+    private List<CoinObserver> observers = new ArrayList<>();
 
     public static CoinFlipManager getInstance() {
         if (instance == null) {
@@ -40,24 +25,21 @@ public class CoinFlipManager {
         return instance;
     }
 
-
-    public interface CoinObserver {
-        void notifyCounterChanged();
-    }
-
     public void registerChangeCallback(CoinObserver obs) {
         observers.add(obs);
     }
+
     public void unRegisterChangeCallback(CoinObserver obs) {
         observers.remove(obs);
     }
+
     private void notifyValueHasChanged() {
         for (CoinObserver obs : observers) {
             obs.notifyCounterChanged();
         }
     }
 
-    public String flipRandomCoin(String userChoice){
+    public String flipRandomCoin(String userChoice) {
 
         Random rand = new Random();
         int randomInt = rand.nextInt(2);
@@ -74,69 +56,57 @@ public class CoinFlipManager {
                 throw new IllegalStateException("Unexpected value: " + randomInt);
         }
 
+        saveCoinFlip(result, userChoice, getNextChild());
+        notifyValueHasChanged();
+
+        return result;
+    }
+
+    @Nullable
+    private String getNextChild() {
         String childPick;
-
-        if (childManager.getAll().size()==0) {
+        if (childManager.getAll().size() == 0) {
             childPick = null;
-        }
-        else {
-            childPick = childManager.get(nextChild).getName();
-            if (childManager.getAll().size() <= nextChild+1){
-                this.nextChild = 0;
-            }
-            else {
-                this.nextChild = this.nextChild + 1;
+        } else {
+            childPick = childManager.get(childIndex).getName();
+            if (childManager.getAll().size() <= childIndex + 1) {
+                this.childIndex = 0;
+            } else {
+                this.childIndex = this.childIndex + 1;
             }
         }
-
-        saveCoinFlip(result,userChoice, childPick);
-
-        notifyValueHasChanged();
-
-        return result;
+        return childPick;
     }
 
-    public String emptyFlip(){
-        Random rand = new Random();
-        int randomInt = rand.nextInt(2);
-        String result;
-
-        switch (randomInt) {
-            case 0:
-                result = "Heads";
-                break;
-            case 1:
-                result = "Tails";
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + randomInt);
-        }
-        notifyValueHasChanged();
-        return result;
-    }
-
-    public void saveCoinFlip(String result, String userChoice, String childPicked){
+    public void saveCoinFlip(String result, String userChoice, String childPicked) {
 
         LocalDateTime creationTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         String time = creationTime.format(formatter);
 
-        Coin coinflip = new Coin(time, childPicked, userChoice, result);
-        coinFlipHistory.add(0,coinflip);
+        Coin coinGame = new Coin(time, childPicked, userChoice, result);
+        coinFlipHistory.add(0, coinGame);
 
     }
 
-    public Child getLastChildPicked(){
-        ChildManager childManager = ChildManager.getInstance();
-        this.nextChild = this.nextChild + 1;
-        return childManager.get(this.nextChild-1);
-    }
-
-    public Coin getCoinFlipGame(int index){
+    public Coin getCoinFlipGame(int index) {
         return this.coinFlipHistory.get(index);
     }
 
-    public ArrayList<Coin> getCoinList() { return this.coinFlipHistory; }
+    public ArrayList<Coin> getCoinList() {
+        return this.coinFlipHistory;
+    }
 
-    public int currentGamesPlayed(){ return this.coinFlipHistory.size()-1; }
+    public int getChildIndex() {
+        return childIndex;
+    }
+
+    public void setChildIndex(int childIndex) {
+        this.childIndex = childIndex;
+    }
+
+    public interface CoinObserver {
+        void notifyCounterChanged();
+    }
+
 }
