ea8f118202b27a6973f59946c060c752acecd4ab
Lee.I <6youwole@gmail.com>
2021-11-08 04:08:36 -0800

    Implement alert and notification.

--- app/src/main/java/ca/sfu/cmpt276/be/parentapp/model/TimeoutManager.java
+++ app/src/main/java/ca/sfu/cmpt276/be/parentapp/model/TimeoutManager.java
@@ -1,10 +1,37 @@
 package ca.sfu.cmpt276.be.parentapp.model;
 
-import android.os.CountDownTimer;
-
 public class TimeoutManager {
     private static TimeoutManager instance;
-    private CountDownTimer countDownTimer;
+    private boolean isTimerRunning = false;
+    private boolean firstState = true;
+    private boolean isClickedNotification = false;
+
+    public boolean isClickedNotification() {
+        return isClickedNotification;
+    }
+
+    public void setClickedNotification(boolean clickedNotification) {
+        isClickedNotification = clickedNotification;
+    }
+
+    private long timeChosen = 0;
+    private long tempTime = 0;
+
+    public long getTimeChosen() {
+        return timeChosen;
+    }
+
+    public void setTimeChosen(long timeChosen) {
+        this.timeChosen = timeChosen;
+    }
+
+    public long getTempTime() {
+        return tempTime;
+    }
+
+    public void setTempTime(long tempTime) {
+        this.tempTime = tempTime;
+    }
 
     public static TimeoutManager getInstance(){
         if(instance == null) {
@@ -13,11 +40,19 @@ public class TimeoutManager {
         return instance;
     }
 
-    public CountDownTimer getCountDownTimer() {
-        return countDownTimer;
+    public boolean isTimerRunning() {
+        return isTimerRunning;
     }
 
-    public void setCountDownTimer(CountDownTimer countDownTimer) {
-        this.countDownTimer = countDownTimer;
+    public void setTimerRunning(Boolean isTimerRunning) {
+        this.isTimerRunning = isTimerRunning;
+    }
+
+    public boolean isFirstState() {
+        return firstState;
+    }
+
+    public void setFirstState(boolean firstState) {
+        this.firstState = firstState;
     }
 }
