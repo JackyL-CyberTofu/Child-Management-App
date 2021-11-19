3d5dc8b626a7af8ca355a152f6e434fb3b962789
rletran <rletran@sfu.ca>
2021-11-18 16:20:19 -0800

    Refactor

--- app/src/main/java/ca/sfu/cmpt276/be/parentapp/view/ChildEditActivity.java
+++ app/src/main/java/ca/sfu/cmpt276/be/parentapp/view/ChildEditActivity.java
@@ -80,6 +80,23 @@ public class ChildEditActivity extends AppCompatActivity implements View.OnClick
         super.finish();
     }
 
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.button_add_image){
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            imageOfChild.setImageURI(selectedImage);
+        }
+    }
+
     private void deleteChild() {
         ChildManager childManager = new ChildManager();
         childManager.remove(childPosition);
@@ -139,26 +156,9 @@ public class ChildEditActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
-    public void getGalleryExtraction() {
-        imageOfChild = (ImageView)findViewById(R.id.image_x);
-        changeImage = (Button)findViewById(R.id.button_x);
+    private void getGalleryExtraction() {
+        imageOfChild = findViewById(R.id.image_child_portrait);
+        changeImage = findViewById(R.id.button_add_image);
         changeImage.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.button_x){
-            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
-            Uri selectedImage = data.getData();
-            imageOfChild.setImageURI(selectedImage);
-        }
     }
 }
